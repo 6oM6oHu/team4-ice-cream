54c73cf3b1d5915cf3092d41624900b4c8176c3b
Anna Kolodiichyk <sianna878@gmail.com>
2022-12-25 05:02:16 +0200

    add gallery

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,7 +1,18 @@
-$(".autoplay-customers").slick({
-    dots: true,
+$('.autoplay-customers').slick({
+  dots: true,
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 5000,
+});
+
+// section gallery
+
+$('.autoplay-gallery').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  fade: true,
+  cssEase: 'linear',
+  autoplay: true,
+  autoplaySpeed: 6000,
 });
