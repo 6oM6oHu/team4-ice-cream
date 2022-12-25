e84332d9df1e2f921bc44d6543a192b34d6f45e8
6oM6oHu <114534968+6oM6oHu@users.noreply.github.com>
2022-12-25 08:01:35 +0000

    Deploying to gh-pages from @ 6oM6oHu/team4-ice-cream@118a61dd17d9d7b5b704da072715985830b884ad 🚀

---
+++ index.e9160d10.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".animate-wrapper");new IntersectionObserver((function(e){e.forEach((function(e){var t=e.target.querySelector(".animate");("function"!=typeof getCurrentAnimationPreference||getCurrentAnimationPreference())&&(e.isIntersecting?t.classList.add("start-animation"):t.classList.remove("start-animation"))}))})).observe(e)}();
+//# sourceMappingURL=index.e9160d10.js.map
