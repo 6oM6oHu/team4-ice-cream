61088958493514b781111f0b980f42d81e2b8108
6oM6oHu <114534968+6oM6oHu@users.noreply.github.com>
2022-12-27 11:21:08 +0000

    Deploying to gh-pages from @ 6oM6oHu/team4-ice-cream@f79c747eda011c4956b58b7936a84f45fe9de4d8 🚀

--- index.4be657d1.js
+++
@@ -1,2 +0,0 @@
-$(".autoplay-customers").slick({dots:!0,slidesToShow:1,slidesToScroll:1,autoplay:!0,autoplaySpeed:5e3}),$(".autoplay-gallery").slick({slidesToShow:1,slidesToScroll:1,fade:!0,cssEase:"linear",autoplay:!0,autoplaySpeed:6e3});
-//# sourceMappingURL=index.4be657d1.js.map
---
+++ index.c9cdc298.js
@@ -0,0 +1,2 @@
+$(".autoplay-customers").slick({dots:!0,slidesToShow:1,slidesToScroll:1,autoplay:!0,autoplaySpeed:5e3}),$(".autoplay-gallery").slick({slidesToShow:1,slidesToScroll:1,fade:!0,autoplay:!0,autoplaySpeed:6e3});
+//# sourceMappingURL=index.c9cdc298.js.map
