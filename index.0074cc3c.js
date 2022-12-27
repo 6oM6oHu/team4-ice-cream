a5c3cbcf0496c6485a8abfccc069a44bdd9c09c8
6oM6oHu <114534968+6oM6oHu@users.noreply.github.com>
2022-12-27 09:06:34 +0000

    Deploying to gh-pages from @ 6oM6oHu/team4-ice-cream@3d4744bf4c8e6cf0cb275db21b0e61b359b82071 🚀

--- index.0074cc3c.js
+++ index.0074cc3c.js
@@ -1,2 +1,2 @@
 !function(){var e;e=document.querySelector(".animate-wrapper"),new IntersectionObserver((function(e){e.forEach((function(e){var t=e.target.querySelector(".animate");("function"!=typeof getCurrentAnimationPreference||getCurrentAnimationPreference())&&(e.isIntersecting?t.classList.add("start-animation"):t.classList.remove("start-animation"))}))})).observe(e)}();
-//# sourceMappingURL=index.01abfc00.js.map
+//# sourceMappingURL=index.0074cc3c.js.map
--- index.068af32b.js
+++ index.068af32b.js
@@ -1,2 +1,2 @@
 var e;e=document.querySelector(".animate-wrapper"),new IntersectionObserver((e=>{e.forEach((e=>{var t=e.target.querySelector(".animate");("function"!=typeof getCurrentAnimationPreference||getCurrentAnimationPreference())&&(e.isIntersecting?t.classList.add("start-animation"):t.classList.remove("start-animation"))}))})).observe(e);
-//# sourceMappingURL=index.2487e34e.js.map
+//# sourceMappingURL=index.068af32b.js.map
