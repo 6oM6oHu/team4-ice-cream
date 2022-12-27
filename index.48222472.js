0d65a68e521a17569d30d38a706bef0a1b679aae
6oM6oHu <114534968+6oM6oHu@users.noreply.github.com>
2022-12-27 08:04:38 +0000

    Deploying to gh-pages from @ 6oM6oHu/team4-ice-cream@871e59749ecedba1e85d67c21f6619ae8eed2ace 🚀

---
+++ index.48222472.js
@@ -0,0 +1,2 @@
+var refs={headerEl:document.querySelector(".header-js")};window.onscroll=function(){window.screen.width>=768?scrollY<=70?refs.headerEl.classList.remove("header-bg"):refs.headerEl.classList.add("header-bg"):scrollY<=120?refs.headerEl.classList.remove("header-bg"):refs.headerEl.classList.add("header-bg")};
+//# sourceMappingURL=index.48222472.js.map
